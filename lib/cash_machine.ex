7619a9bd2202f15a0d763972198ac0ca4431c5c6
0legIv <olegiivnv@gmail.com>
2019-10-08 12:17:27 +0300

    Initialized the project

---
+++ lib/cash_machine.ex
@@ -0,0 +1,18 @@
+defmodule CashMachine do
+  @moduledoc """
+  Documentation for CashMachine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CashMachine.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
